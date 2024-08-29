129e19e706620c2e0529b8114fa8639435760537
Julia Naimovich <naimovich_julia@mail.ru>
2024-08-29 19:55:48 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+
+    @Test
+    public void addNewContactTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        int contactsQuantity = phoneBook.add("Anna", "88005553535");
+        assertEquals(1, contactsQuantity);
+    }
+
+    @Test
+    public void addExistentContactTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Anna", "88005553535");
+        int contactsQuantity = phoneBook.add("Anna", "88005553535");
+        assertEquals(1, contactsQuantity);
+    }
+
+    @Test
+    public void addDifferentContactsTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Anna", "88005553535");
+        int contactsQuantity = phoneBook.add("Elena", "88166544514");
+        assertEquals(1, contactsQuantity);
+    }
 }
