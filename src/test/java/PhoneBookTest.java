67803c72a5bda5d70820a25c661e55b17e1c86d9
Julia Naimovich <naimovich_julia@mail.ru>
2024-08-29 20:11:40 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -4,26 +4,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
 
+    String annaName = "Anna";
+    String annaNumber = "88005553535";
+    String elenaName = "Elena";
+    String elenaNumber = "88166544514";
+
     @Test
     public void addNewContactTest() {
         PhoneBook phoneBook = new PhoneBook();
-        int contactsQuantity = phoneBook.add("Anna", "88005553535");
+        int contactsQuantity = phoneBook.add(annaName, annaNumber);
         assertEquals(1, contactsQuantity);
     }
 
     @Test
     public void addExistentContactTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Anna", "88005553535");
-        int contactsQuantity = phoneBook.add("Anna", "88005553535");
+        phoneBook.add(annaName, annaNumber);
+        int contactsQuantity = phoneBook.add(annaName, annaNumber);
         assertEquals(1, contactsQuantity);
     }
 
     @Test
     public void addDifferentContactsTest() {
         PhoneBook phoneBook = new PhoneBook();
-        phoneBook.add("Anna", "88005553535");
-        int contactsQuantity = phoneBook.add("Elena", "88166544514");
+        phoneBook.add(annaName, annaNumber);
+        int contactsQuantity = phoneBook.add(elenaName, elenaNumber);
         assertEquals(2, contactsQuantity);
+    }
+
+    @Test
+    public void findContactNameByExistentNumberTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add(annaName, annaNumber);
+        String contactName = phoneBook.findByNumber(annaNumber);
+        assertEquals(annaName, contactName);
+    }
+
+    @Test
+    public void findContactNameByNonexistentNumberTest() {
+        PhoneBook phoneBook = new PhoneBook();
+        String contactName = phoneBook.findByNumber(annaNumber);
+        assertEquals(annaName, contactName);
     }
 }
